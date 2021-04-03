b050224a8d3caeba2a4c2f3f9f88074244d29fdf
David Ferreira <65172017+tcFeyn@users.noreply.github.com>
2021-04-03 11:11:57 +0000

    Update blhbattery.sh

--- notifications/blhbattery.sh
+++ notifications/blhbattery.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 device=$(bluetoothctl info | grep Alias | cut -d ' ' -f 2,3,4)
-battery=$(bluebattery.py 11:11:22:33:57:65)
+battery=$(bluebattery.py (device MAC))
 blhbat=${battery##* }
 notify-send -u normal "$device" "$blhbat"
