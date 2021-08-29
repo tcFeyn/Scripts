491bf6a89646113a32b941f3a90ec2afc85d4349
David Ferreira <65172017+tcFeyn@users.noreply.github.com>
2021-08-29 13:26:12 +0100

    idk

--- notifications/muted.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sound=$(amixer get Master | grep 'Right:' | awk -F'[][]' '{ print $4 }')
-[ "$sound" = "off" ] && notify-send -u normal -t 600 "Sound Muted" "" --icon="~/.config/dunst/icons/muted.png" || notify-send -u normal -t 600 "Sound Resumed" "" --icon="~/.config/dunst/icons/unmuted.png"
